d0ed55946e65b6135b75993ee95cba18f25b16af
Francisco Hernandez Soto <centenaria@gmail.com>
2026-02-11 10:00:57 -0400

    practica de listas 1

---
+++ index.py
@@ -0,0 +1,12 @@
+numeros = [10, 5, 7, 2 , 1]
+print(f'el valor original de la lista es : {numeros}')
+numeros [0] = 111
+print(f'el valor actual es : {numeros}')
+numeros[1] = numeros[4]
+print(f'el nuevo valor es : {numeros}')
+
+print(f'\nlongitud de la lista : {len(numeros)}')
+
+del(numeros[1])
+print(f'nueva longitud : {len(numeros)}')
+print(f'\n\nnueva lista es : {numeros}')
