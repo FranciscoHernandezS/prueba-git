6e3a699dd35acf1e62241cce89d7b044e284d54c
Francisco Hernandez Soto <centenaria@gmail.com>
2026-02-11 15:40:23 -0400

    listo

--- index.py
+++ index.py
@@ -1,22 +1,59 @@
-# Definición inicial de la lista
-numeros = [10, 5, 7, 2 , 1]
-print(f'el valor original de la lista es : {numeros}')
+# # Definición inicial de la lista
+# numeros = [10, 5, 7, 2 , 1]
+# print(f'el valor original de la lista es : {numeros}')
 
-# Modificación: Se cambia el primer elemento (índice 0)
-numeros [0] = 111
-print(f'el valor actual es : {numeros}')
+# # Modificación: Se cambia el primer elemento (índice 0)
+# numeros [0] = 111
+# print(f'el valor actual es : {numeros}')
 
-# Copia de valor: El valor del índice 4 se asigna al índice 1
-numeros[1] = numeros[4]
-print(f'el nuevo valor es : {numeros}')
+# # Copia de valor: El valor del índice 4 se asigna al índice 1
+# numeros[1] = numeros[4]
+# print(f'el nuevo valor es : {numeros}')
 
-print(f'\nlongitud de la lista : {len(numeros)}')
+# print(f'\nlongitud de la lista : {len(numeros)}')
 
-# Eliminación: Se remueve el elemento en la posición 1, desplazando los demás
-del(numeros[1])
-print(f'nueva longitud : {len(numeros)}')
-print(f'\n\nnueva lista es : {numeros}')
+# # Eliminación: Se remueve el elemento en la posición 1, desplazando los demás
+# del(numeros[1])
+# print(f'nueva longitud : {len(numeros)}')
+# print(f'\n\nnueva lista es : {numeros}')
 
-# Acceso inverso: Uso de índices negativos para leer desde el final
-print(f'{numeros[-1]}')
-print(f'{numeros[-2]}')
+# # Acceso inverso: Uso de índices negativos para leer desde el final
+# print(f'{numeros[-1]}')
+# print(f'{numeros[-2]}')
+
+
+# lista = [1, 2, 3, 4 , 5]
+# print(f'lista actual = {lista}')
+# lista[2] = int(input("Ingrese un numero entero : "))
+# del lista[-1]
+# print(f'lista actualizada : {lista}')
+# print(f"longitud : {len(lista)}")
+
+
+# numeros = [111, 7, 2 ,1]
+
+# numeros.append(4)
+# print(len(numeros))
+# print(f'{numeros}')
+
+# numeros.insert(2, 222)
+# print(f'la nueva longitud es : {len(numeros)}')
+# print(f'la nuevalista es {numeros}')
+
+##########################################################
+
+# mi_lista = []
+
+# for i in range(5):
+#     mi_lista.insert(0,i+1)
+
+# print(f'la lista es : {mi_lista}')
+
+# mi_lista_insert = []
+
+# for r in range(5):
+#     mi_lista_insert.append(r+1)
+
+# print(f'\n\nmi lista insert es : {mi_lista_insert}')
+
+################################################################################
