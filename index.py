6589e6b67226a93961bb42e6ec93c4aaa673f269
Francisco Hernandez Soto <centenaria@gmail.com>
2026-02-11 16:31:01 -0400

    comentado

--- index.py
+++ index.py
@@ -103,11 +103,11 @@
 
 # print(f'la nueva lista es : {mi_lista}')
 
-mi_lista = [1, 2 , 3 , 4 , 5]
-longitud = len(mi_lista)
-print(f'mi lista es : {mi_lista}')
+# mi_lista = [1, 2 , 3 , 4 , 5]
+# longitud = len(mi_lista)
+# print(f'mi lista es : {mi_lista}')
 
-for i in range(longitud // 2):
-    mi_lista[i] , mi_lista[longitud - i - 1] = mi_lista[longitud - i - 1] , mi_lista[i]
+# for i in range(longitud // 2):
+#     mi_lista[i] , mi_lista[longitud - i - 1] = mi_lista[longitud - i - 1] , mi_lista[i]
 
-print(f'\nmi nueva lista es {mi_lista}')
+# print(f'\nmi nueva lista es {mi_lista}')
